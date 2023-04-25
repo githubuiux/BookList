23adb585d78676b3c382c09466690b6707c26e7e
githubuiux <uiuxista@gmail.com>
2023-04-25 22:03:57 +0300

    Initial Commit

---
+++ BookList/BookListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookListApp.swift
+//  BookList
+//
+//  Created by Levent on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
